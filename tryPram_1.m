0a6b4a7cd7eb5f77d29d589f6ba4521c8e62316e
Berk <berkli0511@gmail.com>
2019-01-27 03:13:38 +0800

    simple PID and simple vel limit

--- tryPram_1.m
+++ tryPram_1.m
@@ -17,20 +17,39 @@ function tryPram1()
         vrep.simxSynchronous(clientID,true);
         vrep.simxStartSimulation(clientID,vrep.simx_opmode_blocking);
        
+        P1 = 5;
+        I1 = 0.2;
+        D1 = 0.5;
+        P2 = 5;
+        I2 = 0;
+        D2 = 0;
         
         count = 0;
         state = 0;
+        dt = 0.05;  %time step = 50ms
         diamWheel = 0.1;
         unitVel = 2/diamWheel;  %1/(pi*diamWheel)*(2*pi)
         maxVel = 2*unitVel;
         keepDist = 1;
         leftVel = 0;
         rightVel = 0;
-        dPos = zeros(3);
-        dPosIntegral = zeros(3);
+%         tarLineVel = zeros(3);
+%         tarAngVel = zeros(3);
+%         tipLineVel = zeros(3);
+%         tipAngVel = zeros(3);
+        
+        pos = zeros(3);
+%         posIntegral = zeros(3);
+%         posDiff = zeros(3);
+        pastDist = 0;
+        dist = 0;
+        distIntegral = 0;
+        distDiff = 0;
+        pastTheta = 0;
+        theta = 0;
         thetaIntegral = 0;
-%         dAngle = zeros(3);
-%         dAngleIntegral = zeros(3);
+        thetaDiff = 0;
+        
         proxState = zeros(6);
        
 
@@ -47,21 +66,30 @@ function tryPram1()
        
         vrep.simxSynchronousTrigger(clientID);
         vrep.simxGetObjectPosition(clientID,tar,tip,vrep.simx_opmode_streaming);
-        vrep.simxGetObjectOrientation(clientID,tar,tip,vrep.simx_opmode_streaming);
+%         vrep.simxGetObjectVelocity(clientID,tar,vrep.simx_opmode_streaming);
+%         vrep.simxGetObjectVelocity(clientID,tip,vrep.simx_opmode_streaming);
+        
         for i = 1:6
             vrep.simxReadProximitySensor(clientID,proxSensor(i),vrep.simx_opmode_streaming);
         end
         
         vrep.simxSynchronousTrigger(clientID);
         while toc<100
+             pastDist = dist; 
+             pastTheta = theta;
+             
+             [res,pos] = vrep.simxGetObjectPosition(clientID,tar,tip,vrep.simx_opmode_buffer);
+%              [res,tarLineVel,tarAngVel] = vrep.simxGetObjectVelocity(clientID,tar,vrep.simx_opmode_buffer);
+%              [res,tipLineVel,tipAngVel] = vrep.simxGetObjectVelocity(clientID,tip,vrep.simx_opmode_buffer)
 
-             [res,dPos] = vrep.simxGetObjectPosition(clientID,tar,tip,vrep.simx_opmode_buffer);
-%              [res,dAngle] = vrep.simxGetObjectOrientation(clientID,tar,tip,vrep.simx_opmode_buffer);
-             dPosIntegral = integrator(dPosIntegral,dPos,maxVel);
-             dist = sqrt(dPos(1)^2 + dPos(2)^2);
-             theta = atan2(dPos(2),dPos(1));
-             thetaIntegral = integrator(thetaIntegral,theta,maxVel)
-%              dAngleIntegral = integrator(dAngleIntegral,dAngle,5);
+%              posIntegral = integrator(posIntegral,pos,maxVel);
+%              posDiff = derivator(pastPos,pos,dt,maxVel);
+             dist = sqrt(pos(1)^2 + pos(2)^2);
+             distIntegral = integrator(distIntegral,dist-keepDist,maxVel);
+             distDiff = derivator(pastDist,dist,dt,maxVel);
+             theta = atan2(pos(2),pos(1));
+             thetaIntegral = integrator(thetaIntegral,theta,maxVel);
+             thetaDiff = derivator(pastTheta,theta,dt,maxVel);
              
              for i = 1:6
                  [res,proxState(i)] = vrep.simxReadProximitySensor(clientID,proxSensor(i),vrep.simx_opmode_buffer);
@@ -69,18 +97,40 @@ function tryPram1()
              
              switch state
                  case 0
-                     leftVel = 5*(dist - keepDist) - 5*theta;
-                     rightVel = 5*(dist - keepDist) + 5*theta;
-                     if abs(dist - keepDist) < 0.1 && abs(theta) < 0.05
+                     leftVel =P1*(dist - keepDist) + I1*distIntegral + D1*distDiff  - P2*theta - I2*thetaIntegral - D2*thetaDiff;
+                     rightVel = P1*(dist - keepDist) + I1*distIntegral + D1*distDiff + P2*theta + I2*thetaIntegral - D2*thetaDiff;
+                     [leftVel,rightVel] = velLimit(leftVel,rightVel,maxVel);
+                     if abs(dist - keepDist) < 0.1 && abs(theta) >= 0.05
                          state = 1;
+                         distIntegral = 0;
+                     elseif abs(dist - keepDist) < 0.1 && abs(theta) < 0.05
+                         state = 2;
+                         distIntegral = 0;
+                         thetaIntegral = 0;
                      end
                      
                  case 1
+                     leftVel = -P2*theta - I2*thetaIntegral - D2*thetaDiff;
+                     rightVel = P2*theta + I2*thetaIntegral - D2*thetaDiff;
+                     [leftVel,rightVel] = velLimit(leftVel,rightVel,maxVel);
+                     distIntegral = 0;
+                     if abs(dist - keepDist) >= 0.1
+                         state = 0;
+                     elseif abs(dist - keepDist) < 0.1 && abs(theta) < 0.05
+                         state = 2;
+                         thetaIntegral = 0;
+                     end
+                     
+                 case 2
                      leftVel = 0;
                      rightVel =  0;
+                     distIntegral = 0;
+                     thetaIntegral = 0;
                      if abs(dist - keepDist) >= 0.1 || abs(theta) >= 0.05
                          state = 0;
                      end
+                     toc
+                     break
              end
              vrep.simxSetJointTargetVelocity(clientID,leftMotor,leftVel,vrep.simx_opmode_oneshot);
              vrep.simxSetJointTargetVelocity(clientID,rightMotor,rightVel,vrep.simx_opmode_oneshot);
@@ -106,11 +156,49 @@ end
     
 function matureData = integrator(accum, rawData ,bound)
     matureData = accum + rawData;
-    for i = 1:length(accum)
-        if accum(i) > bound
-            accum(i) = bound;
-        elseif accum(i) < -bound
-            accum(i) = -bound;
+    for i = 1:length(matureData)
+        if matureData(i) > bound
+            matureData(i) = bound;
+        elseif matureData(i) < -bound
+            matureData(i) = -bound;
         end
     end
 end
+
+function resultData = derivator(pastData, presentData ,timeStep,bound)
+    resultData = (presentData-pastData)/timeStep;
+    for i = 1:length(pastData)
+        if resultData(i) > bound
+            resultData(i) = bound;
+        elseif resultData(i) < 0
+            resultData(i) = 0;
+        end
+    end
+end
+
+function [leftVel,rightVel] = velLimit(tempLeftVel,tempRightVel,maxVel)
+    if tempLeftVel == 0 && tempRightVel == 0
+        leftVel = 0 && rightVel == 0;
+    else
+        vel1 = max(tempLeftVel,tempRightVel);
+        vel2 = min(tempLeftVel,tempRightVel);
+        if vel1 > maxVel && abs(vel2) < vel1
+            vel2 = maxVel*vel2/vel1;
+            vel1 = maxVel;
+        elseif vel2 < -maxVel && abs(vel2) >= vel1
+            vel1 = maxVel*vel1/abs(vel2);
+            vel2 = -maxVel;
+        end
+        if tempLeftVel >= tempRightVel
+            leftVel = vel1;
+            rightVel = vel2;
+        else
+            leftVel = vel2;
+            rightVel = vel1;
+        end
+    end
+end
+        
+        
+        
+        
